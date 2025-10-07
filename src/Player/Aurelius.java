ac6c25125be2c9f1abe04a495f2d1c14f4e1c8c2
syos <tolentinopreciousann@gmail.com>
2025-10-07 16:37:27 +0800

    Update Aurelius.java

--- src/Player/Aurelius.java
+++ src/Player/Aurelius.java
@@ -4,6 +4,9 @@ public class Aurelius extends Character{
     public Aurelius(){
         name = "Aurelius";
         instrument = "The Whispering Bulwark";
+        ps.skillName = "Melodic Remedy";
+        ps.skillDescription = "Chords heals 5% of Aurelius’ health\n" +
+                            "After a chord, grants aurelius an extra turn but is limited to use one note.\n";
         setMaxHp(150);
         setHp(getMaxHp());
         setMaxShield(40);
@@ -12,3 +15,4 @@ public class Aurelius extends Character{
         setLevel(1);
     }
 }
+
