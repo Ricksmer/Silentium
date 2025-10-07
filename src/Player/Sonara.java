2ba2d043b52b1a8f4a8632e8f083a7c5cc32440a
syos <tolentinopreciousann@gmail.com>
2025-10-07 16:36:40 +0800

    Update Sonara.java

--- src/Player/Sonara.java
+++ src/Player/Sonara.java
@@ -6,6 +6,8 @@ public class Sonara extends Character implements Dialogue{
     public Sonara(){
         name = "Sonara";
         instrument = "The Scrouge of Echoes";
+        ps.skillName = "Body of Thorns";
+        ps.skillDescription = "Upon receiving damage, The enemy receives 25% of shared true damage from the enemy’s inflicted damage.";
         setMaxHp(150);
         setHp(getMaxHp());
         setMaxShield(35);
@@ -18,3 +20,4 @@ public class Sonara extends Character implements Dialogue{
         System.out.println("HELLO! ");
     }
 }
+
