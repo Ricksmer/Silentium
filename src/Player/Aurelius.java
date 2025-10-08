a1f2741189af6332cec5ecf5a5a28df9585d92d0
Ricksmer <ricksmer.cabatingan219@gmail.com>
2025-10-09 01:24:32 +0800

    Add files via upload

---
+++ src/Player/Aurelius.java
@@ -0,0 +1,17 @@
+package Player;
+
+public class Aurelius extends Character{
+    public Aurelius(){
+        name = "Aurelius";
+        instrument = "The Whispering Bulwark";
+        ps.skillName = "Melodic Remedy";
+        ps.skillDescription = "Chords heals 5% of Aurelius’ health\n" +
+                "After a chord, grants aurelius an extra turn but is limited to use one note.\n";
+        setMaxHp(150);
+        setHp(getMaxHp());
+        setMaxShield(40);
+        //this.ps.Lyron();
+        //this.as.Lyron();
+        setLevel(1);
+    }
+}
