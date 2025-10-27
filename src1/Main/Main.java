a8c7c31b45272292ccb0610d1c4437a706dfebb7
janell <janellmutya@gmail.com>
2025-10-27 23:45:38 +0800

    papaya

--- src1/Main/Main.java
+++ src1/Main/Main.java
@@ -20,8 +20,8 @@ public class Main {
     public static void main(String[] args) {
 
         art.displayTitle();
-        art.sonaraAscii();
         art.lyronAscii();
+        art.sonaraAscii();
         art.aureliusAscii();
 
         player = game.CharacterSelect();
